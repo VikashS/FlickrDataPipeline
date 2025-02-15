ea50215ffaf93a85e01d2e3c388fa59d575705a6
Vikash Singh <glovesdown@gmail.com>
2025-02-15 22:40:30 +0100

    test code

---
+++ sentiment_analysis/scripts/run_processor.py
@@ -0,0 +1,9 @@
+from sentiment_analysis.processor.processor import start_streaming
+
+
+def start_processor():
+    start_streaming()
+
+
+if __name__=="__main__":
+    start_processor()
---
+++ sentiment_analysis/scripts/run_producer.py
@@ -0,0 +1,9 @@
+from sentiment_analysis.producer.producer import start_producer_main
+
+
+def start_producer():
+    start_producer_main()
+
+
+if __name__=="__main__":
+    start_producer()
