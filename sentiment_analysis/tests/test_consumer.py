58125a715493eeb8a85957efe3b56d5399bd0d14
Vikash Singh <glovesdown@gmail.com>
2025-02-15 22:40:13 +0100

    test code

---
+++ sentiment_analysis/tests/test_consumer.py
@@ -0,0 +1,6 @@
+import pytest
+
+def test_process_tags():
+    """Need to implement."""
+    happy_count=2
+    assert happy_count == 2
---
+++ sentiment_analysis/tests/test_producer.py
@@ -0,0 +1,5 @@
+import pytest
+def test_process_tags():
+    """Need to implement."""
+    happy_count=2
+    assert happy_count == 2
